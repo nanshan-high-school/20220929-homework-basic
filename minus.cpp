f85b929882cf541bfdc7aa9d2f8eb976ab0a3847
you-chen <andychou0230411@gmail.com>
2022-10-01 23:44:12 +0800

    955297 依序相減

--- minus.cpp
+++ minus.cpp
@@ -2,12 +2,12 @@
 using namespace std; 
 
 int main() {
-    cout << "請輸入被減數。";
+    cout << "請輸入被減數:";
     int num0, num1, num2, total;
     cin >> num0;
-    cout << "請輸入第一個減數。";
+    cout << "請輸入第一個要減的數:";
     cin >> num1;
-    cout << "請輸入第二個減數。";
+    cout << "請輸入第二個要減的數:";
     cin >> num2;
     total = num0 - num1 - num2;
     cout << num0 <<" - " << num1 << " - " << num2 << " = " << total;
