fc89f4e6488ab0f10f9fd8f39819dbb56a5e4c31
WD-Ray <113119668+WD-Ray@users.noreply.github.com>
2022-10-02 20:35:03 +0800

    Add files via upload

---
+++ 0929計算機.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "輸入要減的三數:\n";
+
+    int num1, num2, num3;
+    cin >> num1 >> num2 >> num3;
+    cout << num1 << " - " << num2 << " - "  << num3 << " = " << num1-num2-num3;
+}
