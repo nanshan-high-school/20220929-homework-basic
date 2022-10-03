6cc234a93421630fb5fe6deea2c65a5b7a2e5af8
samlin <samlin961112@gmail.com>
2022-10-03 18:47:19 +0800

    0929 955196作業

---
+++ 955196/0929 955196作業.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace  std;
+
+int main() {
+    cout << "輸入3個數字";
+    int num1, num2, num3;
+    cin >> num1 >> num2 >> num3;
+    cout <<  num1 - num2 - num3;
+    
+}
+
+
