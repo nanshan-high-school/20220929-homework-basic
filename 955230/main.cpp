3c3a98309c7dc7a113e17e1cee35ecd1f965100c
patrick <patrickchen970625@gmail.com>
2022-10-04 22:39:06 +0800

    33828 955230 陳宣翔

---
+++ 955230/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "給我三個數字\n";
+    
+    int num1, num2, num3;
+    cin >> num1 >> num2 >> num3;
+    cout << "答案" << num1 - num2 -num3;
+}
