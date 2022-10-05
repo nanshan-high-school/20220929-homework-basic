305187c8ff1f4e012bb393d5ba75e4b4e60b4841
chenchen <andychou0230411@gmail.com>
2022-10-05 22:27:31 +0800

    955297 依序相減

--- minus.cpp
+++ minus.cpp
@@ -10,8 +10,8 @@ int main() {
     cout << "Please enter the second number to subtract:";
     cin >> num2;
     total = num0 - num1 - num2;
-    if( num1 < 0 && num2 < 0) cout << num0 <<" - " << "(" << num1 << ")" <<" - " << "(" << num2 << ")" << " = " << total;
-        else if( num1 < 0) cout << num0 <<" - " << "(" << num1 << ")" <<" - " << num2 << " = " << total;
-        else if( num2 < 0) cout << num0 <<" - " << num1 <<" - " << "(" << num2 << ")" << " = " << total;
-        else cout << num0 <<" - " << num1 <<" - " << num2 << " = " << total;
+    if( num1 < 0 && num2 < 0) cout << num0 <<" - " << "(" << num1 << ")" <<" - " << "(" << num2 << ")" << " = " << total; //如果num1與num2都為負數，那num1與num2都要括號//
+        else if( num1 < 0) cout << num0 <<" - " << "(" << num1 << ")" <<" - " << num2 << " = " << total; //如果只有num1小於0，num2大於0，那就只有num1要括號//
+        else if( num2 < 0) cout << num0 <<" - " << num1 <<" - " << "(" << num2 << ")" << " = " << total; //如果只有num2小於0，num1大於0，那就只有num2要括號//
+        else cout << num0 <<" - " << num1 <<" - " << num2 << " = " << total; //其他num1與num2都大於0，則都不用括號//
 }
