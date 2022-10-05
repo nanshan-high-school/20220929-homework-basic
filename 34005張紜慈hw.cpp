437033f4bf39aef086f1cfff7d300f10a037e3c3
anna <changanna0509@gmail.com>
2022-10-05 20:58:07 +0800

    新增34005張紜慈hw

---
+++ 34005張紜慈hw.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "請輸入三個要相減的數字?";
+    
+    int num1, num2, num3;
+    cin >> num1 >> num2 >> num3;
+    
+    cout << "相減結果 num1 - num2 - num3 = " << num1 - num2 - num3;
+}
